ce49e6d18fea60e5359167e40e7c35f39bc37461
Remedan Hyeredin <128710700+RemedanSE123@users.noreply.github.com>
2024-10-23 15:31:25 +0300

    Add files via upload

---
+++ projet 1.sql
@@ -0,0 +1,17 @@
+select *
+from parks_and_recreation.employee_demographics;
+
+select last_name
+from parks_and_recreation.employee_demographics;
+
+
+select last_name,first_name,birth_date
+from parks_and_recreation.employee_demographics;
+
+
+select distinct gender
+from parks_and_recreation.employee_demographics;
+
+
+select last_name,first_name,age,age+100,(age*2)+250-50
+from parks_and_recreation.employee_demographics;
